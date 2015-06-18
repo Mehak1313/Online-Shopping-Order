3d7b8a3886007f892d35dce7a65ae662ee2162ab
Mehak1313 <mehakhafeez@gmail.com>
2015-06-18 12:01:08 +0500

    Create registerform.php

---
+++ registerform.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>REGISTRATION</title>
+</head>
+
+<body  bgcolor="black"    style="color:white;">
+<FORM ACTION="register.php" METHOD=get>
+
+
+<h1>WELCOME to REGISTRATION page...</h1>
+Please input few details below to create an account<br>
+
+	<table border="2">
+	<tr>
+	<td>Enter your Name:</td><td><input name="regname" type="text" size"20"></input></td>
+	</tr>
+	<tr>
+	<td>Enter Email :</td><td><input name="regmail" type="text" size"20"></input></td>
+	</tr>
+	<tr>
+	<td>Enter Password :</td><td><input name="regpass1" type="password" size"20"></input></td>
+	</tr>
+	<tr>
+	<td>Retype password :</td><td><input name="regpass2" type="password" size"20"></input></td>
+	</tr>
+	</table>
+
+<input type="submit" value="register me!"></input>
+
+</FORM>
+
+
+</body>
+</html>
